612df297344e14620d64a7a5565d72fef3024602
ashrafulais <ashraful0103@gmail.com>
2021-05-28 22:47:07 +0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-str-2mu8jy
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-str-2mu8jy)
---
+++ src/app/cart/cart.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items = this.cartService.getItems();
+  checkoutForm = this.formBuilder.group({
+    name: '',
+    address: '',
+    phone: ''
+  });
+
+  constructor(
+    private cartService: CartService,
+    private formBuilder: FormBuilder
+  ) {}
+
+  ngOnInit() {}
+
+  onSubmit(): void {
+    this.items = this.cartService.clearCart();
+    //alert('Your order has been submitted: ' + this.checkoutForm.value);
+    console.warn('Your order has been submitted: ', this.checkoutForm.value);
+    this.checkoutForm.reset();
+  }
+}
